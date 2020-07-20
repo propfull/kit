c01961a3bcc4243b5fb15baca84d960548496847
Saurav Sahu <sauravsahu.12@live.com>
2020-07-20 23:06:46 +0530

    use https://github.com/patrickroberts/svelte-ts-component-template to create a svelte component project

---
+++ src/sample.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  export let name: string;
+</script>
+
+<style>
+  span {
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+      Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+</style>
+
+<svelte:options tag="pf-sample" />
+
+<span>Hello {name}!</span>

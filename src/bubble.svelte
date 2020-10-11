858fa66bb4fe3e39a0ab34b492d3c5934b62c92d
Saurav Sahu <mrsauravsahu@outlook.com>
2020-10-11 12:58:50 +0530

    add a bubble component

---
+++ src/bubble.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import type { Theme } from "../typings/theme";
+  
+    export let theme: Theme = "light";
+</script>
+
+<style>
+  .root {
+    display: inline-block;
+    outline: none;
+    border: 0.25rem solid transparent;
+    background-color: var(--pf__accent);
+    color: var(--pf__font-color);
+    padding: 0.75rem;
+    font-weight: 800;
+    border-radius: 2rem;
+    box-shadow: var(--pf__shadow);
+    transition: box-shadow var(--pf__tn), transform var(--pf__tn),
+      background-color var(--pf__tn);
+  }
+</style>
+
+<div class={`root pf pf-${theme}`}>
+  <slot />
+</div>
---
+++ src/wrappers/bubble.wrapper.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import Bubble from "../bubble.svelte";
+  import { Theme } from "../../typings/theme";
+
+  export let content: any;
+  export let theme: Theme;
+</script>
+
+<Bubble {theme}>
+  {@html content}
+</Bubble>

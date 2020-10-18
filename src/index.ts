b1d9a7ba079a6fc10b26992a09647b4863cd253c
Saurav Sahu <mrsauravsahu@outlook.com>
2020-10-18 16:35:09 +0530

    improv: card and header fixes (#27)
    * add title slot to PfCard
    
    * header fix for invalid type
    
    * export header component

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ import './styles/globals.css';
 export { default as PfSample } from './sample.svelte';
 export { default as PfButton } from './button.svelte';
 export { default as PfCard } from './card.svelte';
+export { default as PfHeader } from './header.internal.svelte';
